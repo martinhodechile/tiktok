c2f1583d6ec73680df2df50ff642fda4f068be0d
martinhodechile <135808880+martinhodechile@users.noreply.github.com>
2023-06-07 10:21:40 +0200

    Update generate.js

--- generate.js
+++ generate.js
@@ -1,27 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD1drgT9OVqfiC4xD4BeUFEpWuEaxuWt_A",
-  authDomain: "tiktok-f1eed.firebaseapp.com",
-  projectId: "tiktok-f1eed",
-  storageBucket: "tiktok-f1eed.appspot.com",
-  messagingSenderId: "594235665146",
-  appId: "1:594235665146:web:e69764d01bddad66927743",
-  measurementId: "G-DLE0BE6F3E"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-
-
 let outfits = [
     {
         title: "Les pépites de l'été sur Amazon",
